e158a6a23da34b303b88657a5a44f23a68ca6620
SamSquare62442 <samdanisamin12@gmail.com>
2020-08-11 01:00:16 +0600

    The primary iteration code sample

--- drafts_secondary.py
+++ drafts_secondary.py
@@ -1,1 +1,22 @@
+class Ranger:
+    def __init__(self,start,end):
+        self.start = start
+        self.end = end
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.start >= self.end:
+            raise StopIteration
+        current = self.start
+        self.start += 1
+        return current
+
+nums = Ranger(1,10)
+for each in nums:
+    print(each)
+
+
+
 
