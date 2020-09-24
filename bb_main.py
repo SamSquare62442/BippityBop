f716002692b8b3a19e824602a227eca2ed06d988
SamSquare62442 <samdanisamin12@gmail.com>
2020-09-24 22:51:54 +0600

    Added a .exe version imported only specific functions from modules

--- bb_main.py
+++ bb_main.py
@@ -1,13 +1,13 @@
-import itertools
-import math
+from itertools import permutations
+from math import factorial
 
 primary_string = input("Enter your word : ")
-print("The word is ",len(primary_string)," characters long and it has ", math.factorial(len(primary_string)),"possible combinations")
+print("The word is ",len(primary_string)," characters long and it has ",factorial(len(primary_string)),"possible combinations")
 primary_input = input("Do you want to see the combinations for the word?(y/n) : ")
 primary_input = primary_input.lower()
 
 if primary_input == "y":
-    result = list(itertools.permutations(primary_string))
+    result = list(permutations(primary_string))
     total_output = []
     for x,item in enumerate(result):
         final_prod = "".join(item)
@@ -27,4 +27,7 @@ if saving_prompt == "y":
 
 else:
     print("Ok!Have a good day!")
+
+
+exit_prompt = input("\nPress any key to exit the program. ")
     
